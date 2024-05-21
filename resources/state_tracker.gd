185b7914f2494f8c9b1d214cc317d304cd47bfe9
Quantum-Cucumber <quantumcucumber@protonmail.com>
2024-05-21 15:48:50 +1000

    More robust undo system

---
+++ resources/state_tracker.gd
@@ -0,0 +1,41 @@
+class_name StateTracker
+extends Node
+
+@export var tracked_properties: Array[NodePath] = []
+
+const GROUP = "state_trackers"
+
+func _ready() -> void:
+	add_to_group(GROUP)
+
+func save_states() -> Dictionary:
+	var state := {}
+
+	for property_path in tracked_properties:
+		state[property_path] = _resolve_nodepath(property_path)
+
+	return state
+
+## Returns the value a given nodepath points to
+func _resolve_nodepath(nodepath: NodePath) -> Variant:
+	var node_component := NodePath(nodepath.get_concatenated_names())
+	var node := get_node(node_component)
+
+	if not is_instance_valid(node): return null
+
+	var subname_component := NodePath(nodepath.get_concatenated_subnames())
+	return node.get_indexed(subname_component)
+
+func restore_state(state: Dictionary) -> void:
+	for property_path in state:
+		_set_nodepath(property_path, state[property_path])
+
+## Sets the property of a nodepath
+func _set_nodepath(nodepath: NodePath, what: Variant) -> void:
+	var node_component := NodePath(nodepath.get_concatenated_names())
+	var node := get_node(node_component)
+
+	if not is_instance_valid(node): return
+
+	var subname_component := NodePath(nodepath.get_concatenated_subnames())
+	node.set_indexed(subname_component, what)
--- resources/undo_manager.gd
+++ resources/undo_manager.gd
@@ -2,25 +2,22 @@ extends Node
 
 @export var player: Player
 
-var action_list: Array[Dictionary] = []
+var states: Array[Dictionary] = []
 
 func _ready():
 	player.did_action.connect(_on_player_did_action)
-	save_states()
 
-func save_states() -> void:
-	var positions := {}
-	for node in get_tree().get_nodes_in_group("track_state"):
-		positions[node] = node.global_position
-	action_list.append(positions)
+# Can't do this in ready because the StateTrackers won't necessarily be in the group yet.
+# If this is linked to a common ancestor's ready signal then it will fire when
+# everything is ready and nodes have been added to groups
+func _on_tree_ready() -> void:
+	# Save all initial states
+	save_states()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("undo"):
-		if player.input_lock:
-			return
-		if action_list.size() <= 1:
-			print("no actions")
-			return
+		if player.input_lock: return
+		if states.size() <= 1: return
 
 		undo_action()
 
@@ -30,9 +27,16 @@ func _unhandled_input(event: InputEvent) -> void:
 func _on_player_did_action():
 	save_states()
 
-func undo_action() -> void:
-	action_list.pop_back()  # Remove the current state
-	var positions: Dictionary = action_list[-1]  # Restore previous state
+func save_states() -> void:
+	var snapshot := {}
+	for state_tracker in get_tree().get_nodes_in_group(StateTracker.GROUP):
+		snapshot[state_tracker] = state_tracker.save_states()
 
-	for node in positions.keys():
-		node.global_position = positions[node]
+	states.append(snapshot)
+
+func undo_action() -> void:
+	states.pop_back()  # Remove the current state
+	var snapshot := states[-1]  # Get previous state
+
+	for state_tracker in snapshot.keys():
+		state_tracker.restore_state(snapshot[state_tracker])
