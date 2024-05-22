9c5c4008dd10c29956dfdadc9260b03ffa922c12
Quantum-Cucumber <quantumcucumber@protonmail.com>
2024-05-22 12:08:10 +1000

    Inverted linked cube preview

--- cubes/inverted_linked/inverted_linked_cube.gd
+++ cubes/inverted_linked/inverted_linked_cube.gd
@@ -1,6 +1,17 @@
 class_name InvertedLinkedCube
 extends Cube
 
+signal player_entered_side(direction)
+signal player_exited_side
+
+func _ready() -> void:
+	super()
+
+	# When the player touches a side sensor, make the other cubes preview that direction
+	for linked_cube in get_linked():
+		player_entered_side.connect(linked_cube.preview_direction)
+		player_exited_side.connect(linked_cube.clear_preview)
+
 # Overrides _push to propagate to make all the cubes move
 func _push(direction: Vector2i):
 	if !can_all_move(-direction):
@@ -12,12 +23,10 @@ func _push(direction: Vector2i):
 
 	super(direction)
 
-
 ## Perform _push as defined in the linked cube
 ## So to not trigger recursive calls
 func base_push(direction: Vector2i):
 	super._push(direction)
-
 
 func can_all_move(direction: Vector2i) -> bool:
 	return get_linked().all(
@@ -30,3 +39,16 @@ func get_linked() -> Array[Node]:
 	# Don't include this cube, since it will move differently
 	linked.erase(self)
 	return linked
+
+func _on_sensor_area_entered(_area: Area2D, direction: Vector2i) -> void:
+	player_entered_side.emit(direction)
+
+func _on_sensor_area_exited(_area: Area2D) -> void:
+	player_exited_side.emit()
+
+func preview_direction(direction: Vector2i) -> void:
+	%DirPreview.position = direction * Global.tile_size
+	%DirPreview.show()
+
+func clear_preview() -> void:
+	%DirPreview.hide()
