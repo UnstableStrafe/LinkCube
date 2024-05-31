fdea754a71082574941d1475c6c9a997e602a4d6
Quantum-Cucumber <quantumcucumber@protonmail.com>
2024-05-31 11:09:21 +1000

    Implement new sprites

--- ui/start_menu/menu.gd
+++ ui/start_menu/menu.gd
@@ -1,6 +1,12 @@
-extends Button
+extends Control
 
 @export var level_select: PackedScene
+@export var settings: PackedScene
 
-func _pressed():
+
+func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_packed(level_select)
+
+func _on_settings_button_pressed() -> void:
+	var settings_scene := settings.instantiate()
+	add_child(settings_scene)
