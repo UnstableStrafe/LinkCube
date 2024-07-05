7c7a4a4b82685eb7b8c0c233188cb55643076f22
Quantum-Cucumber <quantumcucumber@protonmail.com>
2024-07-05 20:17:21 +1000

    SFX bus + slider sound

--- resources/sfx/global_click_sound.gd
+++ resources/sfx/global_click_sound.gd
@@ -1,15 +1,22 @@
-## Injects itself into all buttons that spawn in the node tree
+## Injects click sound into UI items that spawn in the node tree
 extends AudioStreamPlayer
 
-var click :=  preload("res://resources/sfx/click.wav")
+var click := preload("res://resources/sfx/click.wav")
 
 func _enter_tree() -> void:
 	stream = click
+	bus = "SFX"
+
 	get_tree().node_added.connect(_on_Tree_node_added)
 
 func _on_Tree_node_added(node: Node) -> void:
 	if node is BaseButton:
-		node.pressed.connect(func(): _on_Button_clicked(node))
+		node.pressed.connect(_on_Button_clicked)
+	elif node is Slider:
+		node.drag_ended.connect(_on_Slider_changed)
 
-func _on_Button_clicked(_button: BaseButton) -> void:
+func _on_Button_clicked() -> void:
+	play()
+
+func _on_Slider_changed(_value) -> void:
 	play()
